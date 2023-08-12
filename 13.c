f764039660d8c1fe23f5873a31b23502458e5b41
pulkittagrawal <141828109+pulkittagrawal@users.noreply.github.com>
2023-08-12 16:32:14 +0530

    13.c

---
+++ 13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int x, y;
+    printf("put the value of x: ");
+    scanf("%d",&x);
+    printf("put the value of y: ");
+    scanf("%d",&y);
+    printf("value after swapping\n");
+    printf("x=%d and y=%d",y,x);
+}
